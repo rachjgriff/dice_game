16aa3dd9c7b96d4feabd3644d01c68b6c05ec745
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-05-30 11:55:49 +0100

    dice roll method

--- dice_game/spec/dice_spec.rb
+++ dice_game/spec/dice_spec.rb
@@ -5,4 +5,10 @@ describe Dice do
     dice = Dice.new
     expect(dice).to respond_to(:roll)
   end
+
+  it 'rolling dice gives number between 1 and 6' do
+    dice = Dice.new
+    number = dice.roll
+    expect(number).to be_between(1, 6)
+  end
 end
